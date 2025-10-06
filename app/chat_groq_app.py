865b1c2f4ae8a0fe751d9fa6e8c60246bbb74c1f
Nikhil-Mudgal <nmudgal1106@gmail.com>
2025-10-06 17:10:44 +0530

    Switch to supported Groq model

--- app/chat_groq_app.py
+++ app/chat_groq_app.py
@@ -10,7 +10,7 @@ from rag.retriever import retrieve
 
 st.set_page_config(page_title="PortOps Chat", page_icon="⚓", layout="wide")
 
-MODEL = "llama3-8b-4096"
+MODEL = "mixtral-8x7b-32768"
 TEMPERATURE = 0.7
 MAX_TOKENS = 1000
 SYSTEM_PROMPT = """You are PortOps, an assistant for port operations.
